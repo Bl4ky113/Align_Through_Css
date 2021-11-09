ace33fd429d629a267921d4a5b6f1ae04afc88bc
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-11-08 21:49:18 -0500

    Added to keyboard arrow keys alignAll functions

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -1,39 +1,33 @@
 "use strict";
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 // Made By Bl4ky113
 var ALIGN_OBJ = {
   margin: {
-    obj: document.getElementsByClassName("align--margin")[0]
+    obj: document.getElementsByClassName("align--margin")[0],
+    align_status: ["left", "left"]
   },
   table_cell: {
-    obj: document.getElementsByClassName("align--table-cell")[0]
+    obj: document.getElementsByClassName("align--table-cell")[0],
+    align_status: ["left", "left"]
   },
   positions: {
-    obj: document.getElementsByClassName("align--positions")[0]
+    obj: document.getElementsByClassName("align--positions")[0],
+    align_status: ["left", "left"]
   },
   flexbox: {
-    obj: document.getElementsByClassName("align--flexbox")[0]
+    obj: document.getElementsByClassName("align--flexbox")[0],
+    align_status: ["left", "left"]
   },
   grid: {
-    obj: document.getElementsByClassName("align--grid")[0]
+    obj: document.getElementsByClassName("align--grid")[0],
+    align_status: ["left", "left"]
   }
 };
-Object.entries(ALIGN_OBJ).forEach(function (_ref) {
-  var _ref2 = _slicedToArray(_ref, 2),
-      key = _ref2[0],
-      align = _ref2[1];
-
-  align["child"] = align.obj.firstChild;
+Object.values(ALIGN_OBJ).forEach(function (value) {
+  value["child"] = value.obj.firstChild;
 });
 var KEY_CODES = [37, 38, 39, 40];
+var POSIBLE_ALIGN = ["left", "center", "right"];
 
 var alignMargin = function alignMargin() {
   var alignTo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ["center", "center"];
@@ -47,23 +41,25 @@ var alignMargin = function alignMargin() {
   var left_margin = multiplier["".concat(alignTo[0])] * width / 16;
   var top_margin = multiplier["".concat(alignTo[1])] * height / 16;
   ALIGN_OBJ.margin.child.style = "margin-left: ".concat(left_margin, "rem; margin-top: ").concat(top_margin, "rem;");
+  ALIGN_OBJ.margin.align_status = [alignTo[0], alignTo[1]];
 };
 
 var alignTableCell = function alignTableCell() {
-  var alignTo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ["center", "left"];
+  var alignTo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ["center", "center"];
   var text_value = {
     right: "right",
     center: "center",
     left: "left"
   };
   var vertical_value = {
-    right: "top;",
+    right: "bottom; padding-top: 0.25rem;",
     center: "middle; padding-top: 0.15rem;",
-    left: "bottom; padding-top: 0.25rem;"
+    left: "top;"
   };
   var text_align = text_value["".concat(alignTo[0])];
   var vertical_align = vertical_value["".concat(alignTo[1])];
   ALIGN_OBJ.table_cell.obj.style = "text-align: ".concat(text_align, "; vertical-align: ").concat(vertical_align, ";");
+  ALIGN_OBJ.table_cell.align_status = [alignTo[0], alignTo[1]];
 };
 
 var alignPositions = function alignPositions() {
@@ -76,6 +72,7 @@ var alignPositions = function alignPositions() {
   var left_position = align_number["".concat(alignTo[0])] * 100 / 2;
   var top_position = align_number["".concat(alignTo[1])] * 100 / 2;
   ALIGN_OBJ.positions.child.style = "left: ".concat(left_position, "%; top: ").concat(top_position, "%; transform: translate(-").concat(left_position, "%, -").concat(top_position, "%);");
+  ALIGN_OBJ.positions.align_status = [alignTo[0], alignTo[1]];
 };
 
 var alignFlexbox = function alignFlexbox() {
@@ -88,6 +85,7 @@ var alignFlexbox = function alignFlexbox() {
   var justify_content = flex_value["".concat(alignTo[0])];
   var align_items = flex_value["".concat(alignTo[1])];
   ALIGN_OBJ.flexbox.obj.style = "justify-content: ".concat(justify_content, "; align-items: ").concat(align_items, ";");
+  ALIGN_OBJ.flexbox.align_status = [alignTo[0], alignTo[1]];
 };
 
 var alignGrid = function alignGrid() {
@@ -100,9 +98,155 @@ var alignGrid = function alignGrid() {
   var column_cell = grid_cell["".concat(alignTo[0])];
   var row_cell = grid_cell["".concat(alignTo[1])];
   ALIGN_OBJ.grid.child.style = "grid-column: ".concat(column_cell, "; grid-row: ").concat(row_cell, ";");
+  ALIGN_OBJ.grid.align_status = [alignTo[0], alignTo[1]];
 };
 
-window.onload = function () {}; // window.onkeydown = (event) => {
-//   if (KEY_CODES.includes(event.keyCode)) {
-//   }
-// }
+var alignAll = function alignAll() {
+  var alignTo = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : ["center", "center"];
+  alignMargin(alignTo);
+  alignTableCell(alignTo);
+  alignPositions(alignTo);
+  alignFlexbox(alignTo);
+  alignGrid(alignTo);
+};
+
+var checkGlobalAlign = function checkGlobalAlign() {
+  var global_align = {
+    x: [],
+    y: []
+  };
+  Object.values(ALIGN_OBJ).forEach(function (value) {
+    global_align.x.push(value.align_status[0]);
+    global_align.y.push(value.align_status[1]);
+  });
+  var first_x = global_align.x[0];
+  var same_val_x = true;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = global_align.x[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var value = _step.value;
+
+      if (value != first_x) {
+        same_val_x = false;
+        break;
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  var first_y = global_align.y[0];
+  var same_val_y = true;
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = global_align.y[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var _value = _step2.value;
+
+      if (_value != first_y) {
+        same_val_y = false;
+        break;
+      }
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+        _iterator2["return"]();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
+  }
+
+  var return_value = [];
+
+  if (same_val_x === true) {
+    return_value[0] = global_align.x[0];
+  } else {
+    return_value[0] = global_align.x;
+  }
+
+  if (same_val_y === true) {
+    return_value[1] = global_align.y[0];
+  } else {
+    return_value[1] = global_align.y;
+  }
+
+  return return_value;
+};
+
+window.onload = function () {
+  alignAll();
+};
+
+window.onkeydown = function (event) {
+  if (KEY_CODES.includes(event.keyCode)) {
+    var index_key_down = KEY_CODES.indexOf(event.keyCode);
+    var global_align = checkGlobalAlign();
+    var index_global_align = {
+      x: POSIBLE_ALIGN.indexOf(global_align[0]),
+      y: POSIBLE_ALIGN.indexOf(global_align[1])
+    };
+
+    switch (index_key_down) {
+      case 0:
+        if (index_global_align.x > 0) {
+          index_global_align.x -= 1;
+        }
+
+        alignAll([POSIBLE_ALIGN[index_global_align.x], global_align[1]]);
+        break;
+
+      case 1:
+        if (index_global_align.y > 0) {
+          index_global_align.y -= 1;
+        }
+
+        alignAll([global_align[0], POSIBLE_ALIGN[index_global_align.y]]);
+        break;
+
+      case 2:
+        if (index_global_align.x < POSIBLE_ALIGN.length - 1) {
+          index_global_align.x += 1;
+        }
+
+        alignAll([POSIBLE_ALIGN[index_global_align.x], global_align[1]]);
+        break;
+
+      case 3:
+        if (index_global_align.y < POSIBLE_ALIGN.length - 1) {
+          index_global_align.y += 1;
+        }
+
+        alignAll([global_align[0], POSIBLE_ALIGN[index_global_align.y]]);
+        break;
+
+      default:
+        console.error("Error: Invalid Key Down Index");
+        break;
+    }
+
+    console.log(index_global_align);
+  }
+};
--- src/js/script.js
+++ src/js/script.js
@@ -3,26 +3,32 @@
 const ALIGN_OBJ = {
   margin: {
     obj: document.getElementsByClassName("align--margin")[0],
+    align_status: ["left", "left"] 
   },
   table_cell:{ 
-    obj: document.getElementsByClassName("align--table-cell")[0]
+    obj: document.getElementsByClassName("align--table-cell")[0],
+    align_status: ["left", "left"] 
   },
   positions: {
-    obj: document.getElementsByClassName("align--positions")[0]
+    obj: document.getElementsByClassName("align--positions")[0],
+    align_status: ["left", "left"]
   },
   flexbox: {
-    obj: document.getElementsByClassName("align--flexbox")[0]
+    obj: document.getElementsByClassName("align--flexbox")[0],
+    align_status: ["left", "left"]
   },
   grid: {
-    obj: document.getElementsByClassName("align--grid")[0]
+    obj: document.getElementsByClassName("align--grid")[0],
+    align_status: ["left", "left"]
   }
 };
 
-Object.entries(ALIGN_OBJ).forEach(([key, align]) => {
-  align["child"] = align.obj.firstChild;
+Object.values(ALIGN_OBJ).forEach(value => {
+  value["child"] = value.obj.firstChild;
 });
 
-const KEY_CODES = [37, 38 , 39, 40];
+const KEY_CODES = [37, 38, 39, 40];
+const POSIBLE_ALIGN = ["left", "center", "right"];
 
 const alignMargin = (alignTo=["center", "center"]) => {
   const width = ALIGN_OBJ.margin.child.offsetWidth;
@@ -36,26 +42,28 @@ const alignMargin = (alignTo=["center", "center"]) => {
 
   const left_margin = (multiplier[`${alignTo[0]}`] * width) / 16;
   const top_margin = (multiplier[`${alignTo[1]}`] * height) / 16;
-
+  
   ALIGN_OBJ.margin.child.style = `margin-left: ${left_margin}rem; margin-top: ${top_margin}rem;`;
+  ALIGN_OBJ.margin.align_status = [alignTo[0], alignTo[1]];
 };
 
-const alignTableCell = (alignTo=["center", "left"]) => {
+const alignTableCell = (alignTo=["center", "center"]) => {
   const text_value = {
     right: "right",
     center: "center",
     left: "left"
   };
   const vertical_value = {
-    right: "top;",
+    right: "bottom; padding-top: 0.25rem;",
     center: "middle; padding-top: 0.15rem;",
-    left: "bottom; padding-top: 0.25rem;"
+    left: "top;"
   };
 
   const text_align = text_value[`${alignTo[0]}`];
   const vertical_align = vertical_value[`${alignTo[1]}`];
-
+  
   ALIGN_OBJ.table_cell.obj.style = `text-align: ${text_align}; vertical-align: ${vertical_align};`;
+  ALIGN_OBJ.table_cell.align_status = [alignTo[0], alignTo[1]];
 };
 
 const alignPositions = (alignTo=["center", "center"]) => {
@@ -67,8 +75,9 @@ const alignPositions = (alignTo=["center", "center"]) => {
 
   const left_position = (align_number[`${alignTo[0]}`] * 100) / 2;
   const top_position = (align_number[`${alignTo[1]}`] * 100) / 2;
-
+  
   ALIGN_OBJ.positions.child.style = `left: ${left_position}%; top: ${top_position}%; transform: translate(-${left_position}%, -${top_position}%);`;
+  ALIGN_OBJ.positions.align_status = [alignTo[0], alignTo[1]];
 };
 
 const alignFlexbox = (alignTo=["center", "center"]) => {
@@ -82,6 +91,7 @@ const alignFlexbox = (alignTo=["center", "center"]) => {
   const align_items = flex_value[`${alignTo[1]}`];
 
   ALIGN_OBJ.flexbox.obj.style = `justify-content: ${justify_content}; align-items: ${align_items};`;
+  ALIGN_OBJ.flexbox.align_status = [alignTo[0], alignTo[1]];
 };
 
 const alignGrid = (alignTo=["center", "center"]) => {
@@ -95,15 +105,92 @@ const alignGrid = (alignTo=["center", "center"]) => {
   const row_cell = grid_cell[`${alignTo[1]}`];
   
   ALIGN_OBJ.grid.child.style = `grid-column: ${column_cell}; grid-row: ${row_cell};`;
+  ALIGN_OBJ.grid.align_status = [alignTo[0], alignTo[1]];
+};
+
+const alignAll = (alignTo=["center", "center"]) => {
+  alignMargin(alignTo);
+  alignTableCell(alignTo);
+  alignPositions(alignTo);
+  alignFlexbox(alignTo);
+  alignGrid(alignTo);
+};
+
+const checkGlobalAlign = () => {
+  let global_align = {
+    x: [],
+    y: []
+  };
+
+  Object.values(ALIGN_OBJ).forEach(value => {
+    global_align.x.push(value.align_status[0]);
+    global_align.y.push(value.align_status[1]);
+  });
+
+  let first_x = global_align.x[0];
+  let same_val_x = true;
+  for (let value of global_align.x) {
+    if (value != first_x) {
+      same_val_x = false;
+      break;
+    }
+  }
+
+  let first_y = global_align.y[0];
+  let same_val_y = true;
+  for (let value of global_align.y) {
+    if (value != first_y) {
+      same_val_y = false;
+      break;
+    }
+  }
+
+  let return_value = [];
+  if (same_val_x === true) { return_value[0] = global_align.x[0]; }
+    else { return_value[0] = global_align.x; }
+
+  if (same_val_y === true) { return_value[1] = global_align.y[0]; }
+    else { return_value[1] = global_align.y; }
+
+  return return_value;
 };
 
 window.onload = () => {
-  
+  alignAll();
 };
 
-// window.onkeydown = (event) => {
-//   if (KEY_CODES.includes(event.keyCode)) {
-    
-//   }
-// }
+window.onkeydown = (event) => {
+  if (KEY_CODES.includes(event.keyCode)) {
+    const index_key_down = KEY_CODES.indexOf(event.keyCode);
+    const global_align = checkGlobalAlign();
+    const index_global_align = {
+      x: POSIBLE_ALIGN.indexOf(global_align[0]),
+      y: POSIBLE_ALIGN.indexOf(global_align[1])
+    };
+
+    switch (index_key_down) {
+      case 0:
+        if (index_global_align.x > 0) { index_global_align.x -= 1; }
+        alignAll([POSIBLE_ALIGN[index_global_align.x], global_align[1]])
+        break;
+      case 1:
+        if (index_global_align.y > 0) { index_global_align.y -= 1; }
+        alignAll([global_align[0], POSIBLE_ALIGN[index_global_align.y]])
+        break;
+      case 2:
+        if (index_global_align.x < (POSIBLE_ALIGN.length - 1)) { index_global_align.x += 1; }
+        alignAll([POSIBLE_ALIGN[index_global_align.x], global_align[1]])
+        break;
+      case 3:
+        if (index_global_align.y < (POSIBLE_ALIGN.length - 1)) { index_global_align.y += 1; }
+        alignAll([global_align[0], POSIBLE_ALIGN[index_global_align.y]])
+        break;
+      default:
+        console.error("Error: Invalid Key Down Index")
+        break;
+    }
+
+    console.log(index_global_align)
+  }
+}
 
